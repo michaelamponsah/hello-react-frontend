5587a4407c800ae590c13703bd965781eec14fdc
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-26 18:45:53 +0000

    Dispatch fetchGreeting() action on component mount

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../../redux/Greeting/greetingSlice';
 import './style.css';
 
-const Greeting = () => (
-  <div className="message">Greeting</div>
-);
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greeting.greeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  return (
+    <div className="message">{greeting}</div>
+  );
+};
 
 export default Greeting;
